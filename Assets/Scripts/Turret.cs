05f2f5ad8ed547cf70e19d6d878fe9da8257f0e0
L00Pssss <ggstyle20@gmail.com>
2023-07-20 14:10:06 +0300

    Create Turret

---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,46 @@
+using Mirror;
+using UnityEngine;
+
+public class Turret : NetworkBehaviour
+{
+    [SerializeField] private GameObject m_Projectile;
+
+    [SerializeField] private float m_FireRate;
+
+    private float currentTime;
+
+    private void Update()
+    {
+        if (isServer)
+        {
+            currentTime += Time.deltaTime;
+        }
+    }
+
+    [Command]
+    public void CmdFire()
+    {
+        SvFire();
+    }
+
+    [Server]
+    private void SvFire()
+    {
+        if (currentTime < m_FireRate) return;
+
+        GameObject projectile = Instantiate(m_Projectile, transform.position, Quaternion.identity);
+        projectile.GetComponent<Projectile>().SetParent(transform);
+
+        currentTime = 0;
+
+        RpsFire();
+    }
+
+    [ClientRpc]
+    private void RpsFire()
+    {
+        GameObject projectile = Instantiate(m_Projectile, transform.position, Quaternion.identity);
+        projectile.GetComponent<Projectile>().SetParent(transform);
+    }
+
+}
