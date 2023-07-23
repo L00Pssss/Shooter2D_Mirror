fa46969c49bf4740f482ef535bfc895d4d138302
L00Pssss <ggstyle20@gmail.com>
2023-07-23 17:09:32 +0300

    Fix  Color for tank.

--- Assets/Scripts/VehicleColor.cs
+++ Assets/Scripts/VehicleColor.cs
@@ -1,15 +1,31 @@
 using Mirror;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class VehicleColor : NetworkBehaviour
 {
     [SerializeField] 
-    private SpriteRenderer m_SpriteRenderer;
+    private List<SpriteRenderer> m_SpriteRenderer;
     [SerializeField]
     private Vehicle m_Vehicle;
 
     private void Start()
     {
-        m_SpriteRenderer.color = m_Vehicle.Owner.GetComponent<Player>().PlayerColor;
+        SetVehicleColor();
+    }
+
+    private void SetVehicleColor()
+    {
+
+        if (m_SpriteRenderer == null || m_SpriteRenderer.Count == 0 || m_Vehicle == null)
+            return;
+
+        Color playerColor = m_Vehicle.Owner.GetComponent<Player>().PlayerColor;
+
+
+        foreach (SpriteRenderer renderer in m_SpriteRenderer)
+        {
+            renderer.color = playerColor;
+        }
     }
 }
